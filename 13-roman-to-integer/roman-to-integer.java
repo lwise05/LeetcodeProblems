4b46008e27267a13cc03fa88fd456064338b181b
lwise05 <leslie_aldaz@hotmail.com>
2025-05-30 15:17:32 -0600

    Time: 5 ms (53.57%) | Memory: 44.4 MB (98.52%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,24 +1,29 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character,Integer> romanNum= new HashMap<>();
-        romanNum.put('I', 1);
-        romanNum.put('V', 5);
-        romanNum.put('X', 10);
-        romanNum.put('L', 50);
-        romanNum.put('C', 100);
-        romanNum.put('D', 500);
-        romanNum.put('M', 1000);
-
+        char[] charArray = s.toCharArray();
         int total = 0;
 
-         for(int i = 0 ; i < s.length() ; i++){
-            if(i < s.length() - 1 && romanNum.get(s.charAt(i)) < romanNum.get(s.charAt(i + 1))){
-                total -= romanNum.get(s.charAt(i));
+        Map<Character, Integer> numeralValues = new HashMap<>();
+        numeralValues.put('I', 1);
+        numeralValues.put('V', 5);
+        numeralValues.put('X', 10);
+        numeralValues.put('L', 50);
+        numeralValues.put('C', 100);
+        numeralValues.put('D', 500);
+        numeralValues.put('M', 1000);
+
+        int length = charArray.length -1;
+        for (int i = 0; i <= length ; i++) {
+            int currentValue = numeralValues.get(charArray[i]);
+            int nextValue = (i + 1 < charArray.length) ? numeralValues.get(charArray[i+1]) : 0;
+
+            if (currentValue < nextValue) {
+                total -= currentValue;
             }
             else {
-                total += romanNum.get(s.charAt(i));
+                total += currentValue;
             }
-         }
-         return total;
+        }
+        return total;
     }
 }
